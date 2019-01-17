029682bbb1b8f34ccc15de76137ddd0c9e33de25
S <ssv273@mail.ru>
2019-01-17 15:11:00 +0700

    ДЗ_03

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,39 @@
-# 4.	Определить, какое число в массиве встречается чаще всего.
+'''
+Определить, какое число в массиве встречается чаще всего
+'''
+
+import random
+import collections
+
+
+
+lst_base = [random.randint(-100, 100) for i in range(1000)]
+
+# Вариант №1
+print('Вариант №1')
+x = collections.Counter(lst_base).most_common(1)
+print('Число {} встречается {} раз\n'.format(x[0][0],x[0][1]))
+
+# Вариант №2
+print('Вариант №2')
+repeat = []
+global_max_repeat = 0
+global_number = 0
+for el in lst_base:
+    max_repeat = 0
+    number = 0
+    for i in lst_base:
+        if el == i:
+            max_repeat += 1
+            number = el
+    if max_repeat > global_max_repeat:
+        global_max_repeat = max_repeat
+        global_number = el
+for a in range(len(lst_base)):
+    if lst_base[a] == global_number:
+        repeat.append(a)
+print('В данном массиве чаще всего встречается число {}\n'
+      'Оно встречается {} раз\n'
+      'на следующих местах{}'.format(global_number, global_max_repeat, repeat))
+
+
--- Lesson_3/6.py
+++ Lesson_3/6.py
@@ -1,4 +1,21 @@
-"""
-6.	В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами. 
-Сами минимальный и максимальный элементы в сумму не включать.
-"""
+'''
+В одномерном массиве найти сумму элементов, находящихся между минимальным и
+максимальным элементами. Сами минимальный и максимальный элементы в сумму не
+включать.
+'''
+
+import random
+
+lst_base = [random.randint(-100, 100) for i in range(20)]
+print(lst_base)
+
+a_min = lst_base.index(min(lst_base))
+b_max = lst_base.index(max(lst_base))
+
+if a_min > b_max:
+    a_min, b_max = b_max, a_min
+
+summ = 0
+for i in range(a_min + 1, b_max):
+    summ += lst_base[i]
+print('Сумма элементов между {} и {} = {}'.format(lst_base[a_min], lst_base[b_max], summ))

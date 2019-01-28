5c464332bdedd857c022a17ee2051bf76994fb7e
S <ssv273@mail.ru>
2019-01-28 14:55:42 +0800

    ДЗ_06

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,101 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+from memory_profiler import profile
+from pympler import asizeof
+
+
+@profile()
+def finder(n):
+    lst = []
+    for i in range(2, n + 1):
+        for j in range(2, i):
+            if i % j == 0:
+                # если делитель найден, число не простое.
+                break
+        else:
+            lst.append(i)
+    return lst
+
+
+@profile()
+def eratosfen(n):
+    a = [i for i in range(n + 1)]
+    a[1] = 0
+    lst = []
+    i = 2
+    while i <= n:
+        if a[i] != 0:
+            lst.append(a[i])
+            for j in range(i, n + 1, i):
+                a[j] = 0
+        i += 1
+    print(len(lst))
+    return lst
+
+
+a = finder(1000)
+b = eratosfen(1000)
+print(asizeof.asizeof(a))
+print(asizeof.asizeof(b))
+
+# Windows 10, 64-bit
+# Python 3.7
+# первая функция занимает 21,7 мб памяти
+# вторая 21,8
+# список а занимает 3416 b, список b столько же
+
+import numpy
+import random
+
+# Зададим для начала размер матрицы
+
+# a = int(input('Задайте количество строк в матрице '))
+# b = int(input('Задайте количество столбцов в матрице '))
+a = 5
+b = 9
+# Вариант №1
+# Создаем пустой массив
+matrix = numpy.zeros((a, b), int)
+# заполняем его значениями
+for i in range(a):
+    for j in range(b):
+        matrix[i][j] = random.randint(0, 50)
+print(matrix)
+# теперь создаем список из минимальных элементов столбцов
+min_list = []
+for el in range(b):
+    min_list.append(numpy.min(matrix[:, el]))
+print('\n', min_list, 'Это минимальные значения по столбцам')
+# ну и сответственно выводим его
+print('Максимальное значение среди них = ', max(min_list))
+print(asizeof.asizeof(min_list))
+
+# Вариант №2
+
+mat = []
+# Создаем матрицу
+for i in range(a):
+    string = []
+    for j in range(b):
+        string.append(random.randint(0, 50))
+        print('%3d' % string[j], end='')
+    mat.append(string)
+    print('')
+# Создаем список, в который будем заносить минимальные элементы по столбцам
+min_list2 = []
+# определяем минимальные элементы по столбцам
+for i in range(b):
+    min_l = []
+    for j in range(a):
+        min_l.append(mat[j][i])
+    min_list2.append(min(min_l))
+
+print(min_list2, 'минимальные значения по столбцам  ')
+print('Максимальное среди них = {}'.format(max(min_list2)))
+print(asizeof.asizeof(min_list2))
+
+# Windows 10, 64-bit
+# Python 3.7
+# список min_list занимает 248 b, список min_list2 - 232 b
+# странно, но вроде и тот и другой списки, а почему-то первый больше памяти занимает
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,102 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler import asizeof
+
+
+class Person():
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def surname(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+
+class Person1():
+    __slots__ = ['name','job','pay']
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def surname(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'manager', pay)
+
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
+
+
+class Departament:
+    def __init__(self, *args):
+        self.members = list(args)
+
+    def addMember(self, person):
+        self.members.append(person)
+
+    def giveRises(self, percent):
+        for pers in self.members:
+            pers.giveRises(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print(person)
+
+
+if __name__ == '__main__':  # Тестирование.
+    bob = Person('Bob Smith')
+    sue = Person('Suy Jones', job='dev', pay=100000)
+    bob_2 = Person1('Bob Smith')
+    sue_2 = Person1('Suy Jones', job='dev', pay=100000)
+    print('Размер экземпляра класса {} без использования слотов {} bytes'.format(bob.name,asizeof.asizeof(bob)))
+    print('Размер экземпляра класса {} c  использованием слотов {} bytes'.format(bob_2.name,asizeof.asizeof(bob_2)))
+    print('Размер экземпляра класса {} без использования слотов {} bytes'.format(sue.name,asizeof.asizeof(sue)))
+    print('Размер экземпляра класса {} с  использованием слотов {} bytes'.format(sue_2.name,asizeof.asizeof(sue_2)))
+
+'''
+Windows 10, 64-бит
+Python 3.7
+
+Размер экземпляра класса Bob Smith без использования слотов 264 bytes
+Размер экземпляра класса Bob Smith c  использованием слотов 104 bytes
+Размер экземпляра класса Suy Jones без использования слотов 288 bytes
+Размер экземпляра класса Suy Jones с  использованием слотов 128 bytes
+'''
+
+
+'''
+    # print(bob.name, bob.job, bob.pay)
+    # print(sue.surname(), sue.job, sue.pay)
+    # sue.giveRaise(.10)
+    # print(sue.pay)
+    # print(bob.surname())
+    bob.giveRaise(15)
+    print(sue)
+    print(bob)
+    tom = Manager('Tom Jones', 50000)
+    print(tom)
+    tom.giveRaise(0.1)
+    print(tom)
+    print(tom.surname())
+    print('--All three--')
+    for obj in (bob, sue, tom):
+        obj.giveRaise(.10)
+        print(obj)
+    development = Departament(bob, sue)
+    development.addMember(tom)
+    development.showAll()
+    for key in bob.__dict__:
+        print(key, '=>', getattr(bob, key))
+'''
